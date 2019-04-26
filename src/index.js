e47ca752501ce1bc358fe717655d9cf1a9b08f4e
Richard Hughes <eeuc40@hotmail.com>
2019-04-26 21:04:53 +0100

    Use env vars

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 let AWS = require('aws-sdk');
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
-const tableName = 'Latetrains';
 const OK = 200;
+const INTERNAL_SERVER_ERROR = 500;
 
 exports.handler = async (event) => {
     const connectionId = event.requestContext.connectionId;
 
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         this.addItemToDynamo({
             connectionid: connectionId
         }).then(() => {
             resolve({
                 statusCode: OK
             });
+        }).catch(() => {
+            reject({
+                statusCode: INTERNAL_SERVER_ERROR
+            })
         });
     });
 };
 
 exports.addItemToDynamo = (item) => {
     return dynamo.put({
-        TableName: tableName,
+        TableName: process.env.TABLE_NAME,
         Item: item,
     }).promise();
 };
